a8d4bed93473ada3f8a689c4aca4dfa58cc97be2
klhhhhh <1412841649@qq.com>
2025-09-15 17:48:42 -0700

    update install script

---
+++ modular-training/scripts/build/install_scripts.sh
@@ -0,0 +1,7 @@
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+pip install packaging
+pip install wheel
+pip install --no-build-isolation mamba_ssm==2.2.2
+./reinstall.sh
+
+pip install opencc==1.1.6
